6ac2f1362d5f3234d5f04a280be21293152dea48
Louis Waweru <louis@waweru.me>
2023-11-01 08:26:03 -0400

    Shows DragInteraction always works on Windows; Shows a workaround to keep functionality in Windows without affecting Android.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,14 +5,31 @@ public partial class MainPage : ContentPage
 	private bool _isInteracting;
 	private PointF _lastPoint;
 
-	public MainPage() => InitializeComponent();
+	public MainPage()
+	{
+		InitializeComponent();
+
+		/* Workaround for Windows: 
+		 * Uncomment to set up gesture recognizers in code, while not having any in XAML.
+		 * This will allow the functionality on Windows without interfering with Android. 
+#if WINDOWS
+		PointerGestureRecognizer pointerGestureRecognizer = new();
+		pointerGestureRecognizer.PointerMoved += GView_PointerMoved;
+		GView.GestureRecognizers.Add(pointerGestureRecognizer);
+
+		TapGestureRecognizer tapGestureRecognizer = new();
+		tapGestureRecognizer.Tapped += GView_Tapped;
+		GView.GestureRecognizers.Add(tapGestureRecognizer);
+#endif
+		*/
+	}
 
 
 	private void StartInteraction(object sender, TouchEventArgs e)
 	{
 		_isInteracting = true;
-		_lastPoint.X = e.Touches[^1].X;
-		_lastPoint.Y = e.Touches[^1].Y;
+		_lastPoint.X = e.Touches[0].X;
+		_lastPoint.Y = e.Touches[0].Y;
 	}
 
 
@@ -32,13 +49,15 @@ public partial class MainPage : ContentPage
 
 	private void GView_PointerMoved(object sender, PointerEventArgs e)
 	{
-		_lastPoint.X = (float)e.GetPosition(GView).Value.X;
-		_lastPoint.Y = (float)e.GetPosition(GView).Value.Y;
-		StatusLabel.Text = $"X: {_lastPoint.X:N2} Y: {_lastPoint.Y:N2} (GView_PointerMoved)";
+		if (!_isInteracting)
+		{
+			_lastPoint.X = (float)e.GetPosition(GView).Value.X;
+			_lastPoint.Y = (float)e.GetPosition(GView).Value.Y;
+			StatusLabel.Text = $"X: {_lastPoint.X:N2} Y: {_lastPoint.Y:N2} (GView_PointerMoved)";
+		}
 	}
 
 
-	private void GView_Tapped(object sender, TappedEventArgs e) => 
+	private void GView_Tapped(object sender, TappedEventArgs e) =>
 		StatusLabel.Text = $"X: {_lastPoint.X:N2} Y: {_lastPoint.Y:N2} (GView_Tapped)";
 }
-
